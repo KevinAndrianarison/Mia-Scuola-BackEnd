50da248a7a03a177026b8a7e9c3ef8fe00b3c9d3
steeve <kevinandrianarison25@gmail.com>
2024-11-12 06:34:31 +0100

    jaime

--- database/migrations/2024_11_11_091622_create_likes_table.php
+++ database/migrations/2024_11_11_091622_create_likes_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('likes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');;
-            $table->foreignId('annonce_id')->nullable()->constrained()->onDelete('cascade');;
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('annonce_id')->nullable()->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
